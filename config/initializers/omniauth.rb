6d00511fc535c90f45f9d0d3a483f02df50e2f09
Kristine <kteich88@gmail.com>
2016-11-15 15:56:14 -0500

    Identified scope for GitHub OAuth.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, Figaro.env.github_app_id!, Figaro.env.github_app_secret!
+  provider :github, Figaro.env.github_app_id!, Figaro.env.github_app_secret!, scope: 'user,repo'
 end
