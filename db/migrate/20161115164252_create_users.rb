3375aee2a3285b7c235f19eb894bc406905f1e37
Kristine <kteich88@gmail.com>
2016-11-15 11:53:20 -0500

    Updated Users migration.

--- db/migrate/20161115164252_create_users.rb
+++ db/migrate/20161115164252_create_users.rb
@@ -1,6 +1,15 @@
+# Users table polymorphic relationships set through account_id, account_type
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
+      t.integer :account_id
+      t.string :account_type
+      t.string :code_karma_token
+      t.string :github_token
+      t.json :github_oauth_data
+      t.string :email
+
+      t.timestamp
     end
   end
 end
