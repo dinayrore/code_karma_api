95e643dcf39c4f9a570cde2f8e3ffbf30acdb997
Kristine <kteich88@gmail.com>
2016-11-15 11:57:40 -0500

    Updated Developers migration.

--- db/migrate/20161115164329_create_developers.rb
+++ db/migrate/20161115164329_create_developers.rb
@@ -1,6 +1,9 @@
+# Developers table
 class CreateDevelopers < ActiveRecord::Migration[5.0]
   def change
     create_table :developers do |t|
+      t.integer :user_id, foreign_key: :true
+      t.integer :karma_points
     end
   end
 end
