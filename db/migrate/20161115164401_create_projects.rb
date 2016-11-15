92b47b7e638696c35642ecdc6474807a30308ddf
Kristine <kteich88@gmail.com>
2016-11-15 12:04:11 -0500

    Updated Project migration.

--- db/migrate/20161115164401_create_projects.rb
+++ db/migrate/20161115164401_create_projects.rb
@@ -1,6 +1,15 @@
+# Projects table
 class CreateProjects < ActiveRecord::Migration[5.0]
   def change
     create_table :projects do |t|
+      t.string :title
+      t.string :description
+      t.string :github_repo_url
+      t.string :active_site_url
+      t.boolean :fulfilled
+      t.integer :client_id, foreign_key: true
+
+      t.timestamps
     end
   end
 end
