7cc6db0f952b6e34d6cf83afb838286f77ad3521
Kristine <kteich88@gmail.com>
2016-11-15 12:08:37 -0500

    Updated Developer Project migration.

--- db/migrate/20161115164424_create_developer_projects.rb
+++ db/migrate/20161115164424_create_developer_projects.rb
@@ -1,6 +1,11 @@
+# Developer Projects Table holds foreign keys for projects and developers
 class CreateDeveloperProjects < ActiveRecord::Migration[5.0]
   def change
     create_table :developer_projects do |t|
+      t.integer :project_id, foreign_key: true
+      t.integer :developer_id, foreign_key: true
+
+      t.timestamps
     end
   end
 end
