9896d9ed86e2777954b7bda0fc9bcfe3b0a8d036
Tim <timothyhsieh27@gmail.com>
2016-11-15 14:43:44 -0500

    Created Sessions controller with initial update and create functionality.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+#
+class SessionsController < ApplicationRecord
+  skip_before_action :check_user, only: [:create]
+  def create
+binding.pry
+    data = request.env['omniauth.auth']
+
+    user = User.find_by email: data.info.email
+    if user
+      user.update!(
+        github_token:      data.credentials.token,
+        github_oauth_data: data.to_json
+      )
+    else
+      user = User.create!(
+
+        account_type:      data.account,
+        code_karma_token:  SecureRandom.uuid,
+        github_token:      data.credentials.token,
+        github_oauth_data: data.to_json,
+        email:             data.info.email
+      )
+    end
+
+    redirect_to "https://samanthasheadavis.github.io/codeKarma/#/redirect/#{user.code_karma_token}"
+  end
+end
