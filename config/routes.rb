baefcaf1c02289d21a97ee9665837d933c56e753
Kristine <kteich88@gmail.com>
2016-11-15 15:34:18 -0500

    Created route for callback using sessions#create.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'users#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/auth/github/callback', to: 'sessions#create'
 end
