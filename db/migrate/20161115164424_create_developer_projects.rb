7b0c604e41b096fd5ef29a7a5d063b557dffd9a9
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-15 13:56:02 -0500

    brief description/projects, perc comp, est comp time

--- db/migrate/20161115164424_create_developer_projects.rb
+++ db/migrate/20161115164424_create_developer_projects.rb
@@ -2,6 +2,8 @@
 class CreateDeveloperProjects < ActiveRecord::Migration[5.0]
   def change
     create_table :developer_projects do |t|
+      t.string :percentage_complete
+      t.string :est_completion_date
       t.integer :project_id, foreign_key: true
       t.integer :developer_id, foreign_key: true
 
@@ -9,3 +11,10 @@ class CreateDeveloperProjects < ActiveRecord::Migration[5.0]
     end
   end
 end
+#percentage complete 0% default the edit
+#completeion date, store
+#completion date edit
+
+#completion date shown
+#percentage complete shown for client
+#developer working on it
