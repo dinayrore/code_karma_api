12e67e0e186c1c9511d63021b9e5050b5bf3749f
Kristine <kteich88@gmail.com>
2016-11-15 16:16:47 -0500

    Updated application controller to verify current user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :check_user
+
+  def current_user
+    @current_user ||= User.find_by api_token: params[:token]
+  end
+
+  def check_user
+    if params[:token].nil?
+      render json: { error: "No Token" }, status: 401
+    elsif current_user.nil?
+      render json: { error: "Invalid Token" }, status: 401
+    end
+  end
 end
