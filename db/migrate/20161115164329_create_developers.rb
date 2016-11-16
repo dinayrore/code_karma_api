2afbd37680ae7207a8f0cb226d2235afc9b725c8
Kristine <kteich88@gmail.com>
2016-11-16 09:49:37 -0500

    Eliminated user_id foreign key.

--- db/migrate/20161115164329_create_developers.rb
+++ db/migrate/20161115164329_create_developers.rb
@@ -1,8 +1,7 @@
-# Developers table
+# Developers table, only developers can earn karma points.
 class CreateDevelopers < ActiveRecord::Migration[5.0]
   def change
     create_table :developers do |t|
-      t.integer :user_id, foreign_key: true
       t.integer :karma_points
 
       t.timestamps
