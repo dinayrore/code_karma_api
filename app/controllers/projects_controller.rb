876e3cc73a142135c13a37cd96edc107754df047
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-16 16:19:11 -0500

    controller biz

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,4 +9,17 @@ class ProjectsController < ApplicationController
     @client_projects = Project.where(client_id: @current_user.id)
     render json: @client_projects
   end
+
+  # def create
+  #   @added_project = Project.new(
+  #   params[:title],
+  #   params[:brief_description],
+  #   params[:description],
+  #   params[:github_repo_url]
+  #   )
+  # end
+  #
+  # def update
+  #   @edited_project = Project.find(params[:id])
+  # end
 end
