8bbd7efa1b18168c8366b38a48dcec4ec6557173
Kristine <kteich88@gmail.com>
2016-11-16 10:09:48 -0500

    Added comment to describe relationship.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,4 @@
+# Clients are users through account (polymorphic), they have many projects
 class Client < ApplicationRecord
   has_one :user, :as => :account, dependent: :destroy
   has_many :project
