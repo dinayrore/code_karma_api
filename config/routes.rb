5e686a721268d942feb0f28dcf36a79c500709c1
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-16 14:29:25 -0500

    created route /project/show

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'users#show'
 
   get '/auth/github/callback', to: 'sessions#create'
+
+  get '/projects/show', to: 'projects#show'
+
+  resources :projects, only: [:index]
 end
