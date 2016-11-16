89efc5dd13678ba83979494b2a5c4dfb0c906154
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-16 14:28:25 -0500

    show action created

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,12 @@
-class ProjectsController < ApplicationRecord
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+    @clients = User.where(account_type: "Client")
+    @clients.email
+  end
+
+  def show
+    @client_projects = Project.where(client_id: @current_user.id)
+    render json: @client_projects
+  end
 end
