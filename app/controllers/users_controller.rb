74a77a112aff0bc6414943831ef4e8c7476ab4d6
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-16 14:28:48 -0500

    cleared notes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,4 @@
-class UsersController < ApplicationRecord
-  #   user = User.find_by email: data.info.email
-  #   if Useruser.update(
-  #     github_token:   data.credentials.token,
-  #     github_oath_dtat: data.to_json
-  #   )
-  #   else
-  #     user = User.create!(
-  #     email:  data.info.email,
-  #     api_token: SecureRandom.uuid,
-  #     github_token: data.credentials.token,
-  #     github_oath_data: data.to_json
-  #     )
-  #   end
-  #
-  #   redirect_to "app.example.com/#{user.api_token}"
-  # end
+class UsersController < ApplicationController
   def show
   end
 end
