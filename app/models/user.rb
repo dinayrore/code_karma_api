87a54b6342e937052430af752abc4711c1e42f7f
Kristine <kteich88@gmail.com>
2016-11-16 10:12:26 -0500

    Corrected polymorphic relationship in Users model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 # Users can be Clients or Developers (polymorphic)
 class User < ApplicationRecord
-  belongs_to :account, polymorphic: true, optional: true
+  belongs_to :account, polymorphic: true
 end
