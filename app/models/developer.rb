b400c6e45d78f7dbd5d0160889fba5e3cd6024ec
Kristine <kteich88@gmail.com>
2016-11-16 10:10:21 -0500

    Added comment to describe relationship.

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,3 +1,4 @@
+# Developers are users through account (polymorphic), they have many devprojects
 class Developer < ApplicationRecord
   has_one :user, :as => :account, dependent: :destroy
   has_many :developerproject
