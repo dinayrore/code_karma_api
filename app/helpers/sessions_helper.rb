bd69b1f38137b03b8f9a64c89563c60119fc438c
Tim <timothyhsieh27@gmail.com>
2016-11-16 13:37:50 -0500

    Pulled update and create functionality into sessions helper from sessions controller.

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,34 @@
+#
+module SessionsHelper
+  def update(user, data)
+    user.update!(
+      github_token:      data.credentials.token,
+      github_oauth_data: data.to_json
+    )
+  end
+
+  def create_developer(data)
+    developer = Developer.create!
+    user = User.create!(
+      account:           developer,
+      code_karma_token:  SecureRandom.uuid,
+      github_token:      data.credentials.token,
+      github_oauth_data: data.to_json,
+      email:             data.info.email
+    )
+    user
+  end
+
+  def create_client(data)
+    client = Client.create!
+    user = User.create!(
+      account:           client,
+      code_karma_token:  SecureRandom.uuid,
+      github_token:      data.credentials.token,
+      github_oauth_data: data.to_json,
+      email:             data.info.email
+    )
+    user
+  end
+
+end
