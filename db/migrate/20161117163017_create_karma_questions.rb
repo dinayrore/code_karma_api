b10fd5b5f829511c4225464c859793d8435224a7
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-17 15:30:16 -0500

    add 'question_like' collumn for question like points

--- db/migrate/20161117163017_create_karma_questions.rb
+++ db/migrate/20161117163017_create_karma_questions.rb
@@ -2,7 +2,7 @@ class CreateKarmaQuestions < ActiveRecord::Migration[5.0]
   def change
     create_table :karma_questions do |t|
       t.string :karma_question
-      t.integer :community_like
+      t.integer :question_like
       t.references :developer, index: true, foreign_key: true
 
       t.timestamps
