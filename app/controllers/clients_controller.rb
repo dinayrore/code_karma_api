cf393314eaf4603425b34d5bd296f04491364ea4
Kristine <kteich88@gmail.com>
2016-11-17 15:48:40 -0500

    Updated show method to find user/client id that matched current user by account id.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,6 +1,7 @@
+# OAuth through GitHub to create user/clients
 class ClientsController < ApplicationController
   def show
-    @client = User.find_by(account_id: @current_user.id)
+    @client = User.find_by(account_id: @current_user.account.id)
     @client_dashboard_data = @client.github_oauth_data
     render json: @client_dashboard_data
   end
