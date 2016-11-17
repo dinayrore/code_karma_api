b68e8dc68c1bed3c5fac781dcc06f984369ae10e
Kristine <kteich88@gmail.com>
2016-11-17 11:03:28 -0500

    Updated CRUD functionality for controller for current user only.

--- app/controllers/developer_projects_controller.rb
+++ app/controllers/developer_projects_controller.rb
@@ -1,3 +1,4 @@
+# Show, Create, Update, Delete
 class DeveloperProjectsController < ApplicationController
   def show
     @developer_projects = DeveloperProject.where(developer_id: @current_user.id)
@@ -6,7 +7,7 @@ class DeveloperProjectsController < ApplicationController
 
   def create
     @developer_project = DeveloperProject.new developer_project_params
-    @developer_project.developer_id = current_user.id
+    @developer_project.developer_id = @current_user.id
     if @developer_project.save
       render json: @developer_project
     else
@@ -16,12 +17,18 @@ class DeveloperProjectsController < ApplicationController
 
   def update
     @developer_project = DeveloperProject.find params[:id]
-    @developer_project.developer_id = current_user.id
-    if @developer_project.update developer_project_params
+    if @developer_project.developer_id == @current_user.id
+      @developer_project.update developer_project_params
       render json: @developer_project
     else
       render :json => { :errors => @developer_project.errors.full_messages }, :status => 422
     end
+  end
+
+  def destroy
+    @developer_project.id = DeveloperProject.find params[:id]
+    @developer_project.destroy if @developer_project.developer_id == @current_user.id
+    render json: {}, status: :ok
   end
 
   private
