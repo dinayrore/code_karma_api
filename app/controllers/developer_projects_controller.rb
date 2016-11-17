89a17a0526bb4543d56330bbd66f30fe0eb960b6
Tim <timothyhsieh27@gmail.com>
2016-11-16 20:11:20 -0500

    Added show, create, and update functionality to developer projects controller.

--- app/controllers/developer_projects_controller.rb
+++ app/controllers/developer_projects_controller.rb
@@ -1,12 +1,31 @@
 class DeveloperProjectsController < ApplicationController
+  def show
+    @developer_projects = DeveloperProject.where(developer_id: @current_user.id)
+    render json: @developer_projects
+  end
+
   def create
-    @completion_date = DeveloperProject.new(
-    params[:est_completion_date]
-    )
+    @developer_project = DeveloperProject.new developer_project_params
+    if @developer_project.save
+      render json: @developer_project
+    else
+      render :json => { :errors => @developer_project.errors.full_messages }, :status => 422
+    end
   end
 
   def update
-    @update_project_status = DeveloperProject.find(params[:id])
+    @developer_project = DeveloperProject.find params[:id]
+    if @developer_project.update developer_project_params
+      render json: @developer_project
+    else
+      render :json => { :errors => @developer_project.errors.full_messages }, :status => 422
+    end
   end
-  #lets fill this out tommorow, or yall can get it done tonight.
+
+  private
+
+  def developer_project_params
+    params.permit(:percentage_complete, :est_completion_date,
+                  :project_id, :developer_id)
+  end
 end
