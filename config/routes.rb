a77ad897e4712a2e4141e8dfa3d67424bea4aa42
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-18 11:08:07 -0500

    routes added for total_karma-developer, and karma_questions/karma_comments controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   root 'users#show'
 
   get '/auth/github/callback', to: 'sessions#create'
+  get '/developer/total/user/points', to: 'developer#total_karma'
   post '/projects/:id', to: 'projects#fork'
+  put '/karmaquestion/added_like', to: 'karma_question#added_like'
+  put '/karmacomment/added_like', to: 'karma_comment#added_like'
 
-  resources :developerprojects, only: [:show, :create, :update, :destroy]
+  resources :developer_projects, only: [:show, :create, :update, :destroy]
   resources :projects, only: [:index, :show, :create, :update, :destroy]
   resources :clients, only: [:show]
   resources :developers, only: [:show]
+  resources :karma_question, only: [:index, :create, :update, :destroy]
+  resources :karma_comments, only: [:create, :update, :destroy]
 end
