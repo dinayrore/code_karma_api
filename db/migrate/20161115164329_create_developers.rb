a840fe3383a2638176ac6f6c8e1259ada57dc349
Kristine <kteich88@gmail.com>
2016-11-18 09:50:39 -0500

    Added default 0 to migration.

--- db/migrate/20161115164329_create_developers.rb
+++ db/migrate/20161115164329_create_developers.rb
@@ -2,7 +2,7 @@
 class CreateDevelopers < ActiveRecord::Migration[5.0]
   def change
     create_table :developers do |t|
-      t.integer :karma_points
+      t.integer :karma_points, default: 0
 
       t.timestamps
     end
