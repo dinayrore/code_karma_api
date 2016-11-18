2aa58e5e3a7fb38cb8be5e9aa5e84ebab9f699f5
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-18 10:43:04 -0500

    added underscores to break words in models

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,5 +1,7 @@
 # Developers are users through account (polymorphic), they have many devprojects
 class Developer < ApplicationRecord
+  has_many :karma_question
+  has_many :karma_comment
   has_one :user, :as => :account, dependent: :destroy
-  has_many :developerproject
+  has_many :developer_project
 end
