cea9ba9f2c18a0db0f438cee74d88aa8aca0a74e
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-18 09:09:05 -0500

    karma questions basic crud on controller

--- app/controllers/karma_questions_controller.rb
+++ app/controllers/karma_questions_controller.rb
@@ -1,4 +1,42 @@
 class KarmaQuestionsController < ApplicationController
-  def show
+  def index
+    @show_all_questions = KarmaQuestion.all
+    show_all_questions_and_comments = {}
+    @show_all_questions.each do |question|
+      show_all_questions_and_comments[question] = question.karma_comment
+    end
+    render json: show_all_questions_and_comments
+  end
+
+  def create
+    @new_question = KarmaQuestion.new(
+    karma_question: params[:karma_question],
+    developer_id: @current_user.id
+    )
+    if @new_question.save
+      render json: @new_question
+    end
+  end
+
+  def update
+    @edited_question = KarmaQuestion.find(params[:id])
+    if @edited_question.update(karma_question: params[:karma_question])
+      render json: @edited_question
+    end
+  end
+
+  def added_like
+    @update_question_like_button = KarmaQuestion.find(params[:id])
+    if @update_question_like_button.update(question_like: params[:question_like] + 1)
+      render json: @update_question_like_button
+    end
+  end
+
+  def destroy
+    @deleted_question = KarmaQuestion.find(params[:id])
+    if @deleted_question.developer_id == @current_user.id
+      @deleted_question.destroy
+      render json: {}, status: :ok
+    end
   end
 end
