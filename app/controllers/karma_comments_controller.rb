b5830fe7a78e3e994b5e9f4191825d98c3effa01
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-18 10:42:16 -0500

    karma_comments_controller error message

--- app/controllers/karma_comments_controller.rb
+++ app/controllers/karma_comments_controller.rb
@@ -4,6 +4,8 @@ class KarmaCommentsController < ApplicationController
     @new_comment = @karmaquestion.comments.new(karma_comment: params[:karma_comment])
     if @new_comment.save
       render json: @new_comment
+    else
+      render json: { error: @new_comment.errors.full_messages }, status: 400
     end
   end
 
@@ -11,6 +13,8 @@ class KarmaCommentsController < ApplicationController
     @edited_comment = KarmaComment.find(params[:id])
     if @edited_comment.update(karma_comment: params[:karma_comment])
       render json: @edited_comment
+    else
+      render json: { error: @edited_comment.errors.full_messages }, status: 400
     end
   end
 
@@ -18,6 +22,8 @@ class KarmaCommentsController < ApplicationController
     @update_comment_like_button = KarmaQuestion.find(params[:id])
     if @update_comment_like_button.update(comment_like: params[:comment_like] + 1)
       render json: @update_comment_like_button
+    else
+      render json: { error: @update_comment_like_button.errors.full_messages }, status: 400
     end
   end
 
@@ -26,6 +32,8 @@ class KarmaCommentsController < ApplicationController
     if @deleted_comment.developer_id == @current_user.id
       @deleted_comment.destroy
       render json: {}, status: :ok
+    else
+      render json: { error: @deleted_comment.errors.full_messages }, status: 403
     end
   end
 end
