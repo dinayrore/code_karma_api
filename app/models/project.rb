6b749464aaf1e3b27ea8503bef9a05ae486957b9
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-18 10:44:48 -0500

    added underscores to break words in models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 # Clients own projects and projects may be included in several devprojects
 class Project < ApplicationRecord
   belongs_to :client
-  has_many :developerproject
+  has_many :developer_project
 end
