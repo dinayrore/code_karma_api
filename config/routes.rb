9d0187d33826458beae799276faa71269a64dafc
Tim <timothyhsieh27@gmail.com>
2016-11-17 20:29:04 -0500

    Updated routes to permit Projects Controller Fork action.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'users#show'
 
   get '/auth/github/callback', to: 'sessions#create'
+  post '/projects/:id', to: 'projects#fork'
 
   resources :developerprojects, only: [:show, :create, :update, :destroy]
   resources :projects, only: [:index, :show, :create, :update, :destroy]
