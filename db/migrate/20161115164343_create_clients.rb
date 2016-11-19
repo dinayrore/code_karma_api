e781337ee008b7dcac37d87987eba52c7bae9b31
Kristine <kteich88@gmail.com>
2016-11-18 21:08:49 -0500

    Added organization name and main site url to client.

--- db/migrate/20161115164343_create_clients.rb
+++ db/migrate/20161115164343_create_clients.rb
@@ -2,6 +2,9 @@
 class CreateClients < ActiveRecord::Migration[5.0]
   def change
     create_table :clients do |t|
+      t.string :organization_name
+      t.string :organization_site
+
       t.timestamps
     end
   end
