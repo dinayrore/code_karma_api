42e90394781a569e1dbdd011271dcd4449352fff
Kristine <kteich88@gmail.com>
2016-11-18 21:04:38 -0500

    Made necessary changes to complete functionality of controllers.

--- app/controllers/karma_comments_controller.rb
+++ app/controllers/karma_comments_controller.rb
@@ -1,40 +1,61 @@
 # Community Feed Comments Controller
 class KarmaCommentsController < ApplicationController
-  def create
-    @karmaquestion = KarmaQuestion.find(params[:karmaquestion_id])
-    @new_comment = @karmaquestion.comments.new(karma_comment: params[:karma_comment])
-    if @new_comment.save
-      render json: @new_comment
+  def index
+    @user = @current_user
+    if @user.account_type == 'Developer'
+      @comments = KarmaComment.all
+      binding.pry
+      render :index
     else
-      render json: { error: @new_comment.errors.full_messages }, status: 400
+      render json: { error: 'Incorrect User' }, status: 403
+    end
+  end
+
+  def create
+    @comment = KarmaComment.new comment_params
+    @comment.developer_id = @current_user.account_id
+    if @comment.developer == @current_user.account
+      @comment.save
+      render json: @comment
+    else
+      render json: { errors: 'Semantically Erroneous Instructions' }, status: 422
     end
   end
 
   def update
-    @edited_comment = KarmaComment.find(params[:id])
-    if @edited_comment.update(karma_comment: params[:karma_comment])
-      render json: @edited_comment
+    @comment = KarmaComment.find params[:id]
+    if @comment.developer == @current_user.account
+      @comment.update comment_params
+      render json: @comment
     else
-      render json: { error: @edited_comment.errors.full_messages }, status: 400
-    end
-  end
-
-  def added_like
-    @update_comment_like_button = KarmaQuestion.find(params[:id])
-    if @update_comment_like_button.update(comment_like: params[:comment_like] + 1)
-      render json: @update_comment_like_button
-    else
-      render json: { error: @update_comment_like_button.errors.full_messages }, status: 400
+      render json: { error: 'Incorrect User' }, status: 403
     end
   end
 
   def destroy
-    @deleted_comment = KarmaComment.find params[:id]
-    if @deleted_comment.developer_id == @current_user.id
-      @deleted_comment.destroy
+    @comment = KarmaComment.find params[:id]
+    if @comment.developer == @current_user.account
+      @comment.destroy
       render json: {}, status: :ok
     else
-      render json: { error: @deleted_comment.errors.full_messages }, status: 403
+      render json: { error: 'Incorrect User' }, status: 403
     end
+  end
+
+  def like
+    binding.pry
+    @comment = KarmaComment.find params[:id]
+    if @comment.developer.user.account_type == 'Developer'
+      @comment.update(comment_like: params[:comment_like])
+      render json: @comment
+    else
+      render json: { errors: 'Semantically Erroneous Instructions' }, status: 422
+    end
+  end
+
+  private
+
+  def comment_params
+    params.permit(:karma_comment, :karma_question_id)
   end
 end
