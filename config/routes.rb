6f5ad2b671f0423ea0ee562911757b489001c178
Kristine <kteich88@gmail.com>
2016-11-18 21:07:17 -0500

    Upgraded routes. All routes functioning appropriately with current controller functions.

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   root 'users#show'
 
   get '/auth/github/callback', to: 'sessions#create'
-  get '/developer/total/user/points', to: 'developer#total_karma'
+  get '/developer/total/user/points', to: 'developers#total_karma'
   post '/projects/:id', to: 'projects#fork'
-  put '/karmaquestion/added_like', to: 'karma_question#added_like'
-  put '/karmacomment/added_like', to: 'karma_comment#added_like'
+  put '/karma_question/:id', to: 'karma_questions#like'
+  put '/karma_comment/:id', to: 'karma_comments#like'
 
   resources :developer_projects, only: [:show, :create, :update, :destroy]
   resources :projects, only: [:index, :show, :create, :update, :destroy]
   resources :clients, only: [:show]
   resources :developers, only: [:show]
   resources :karma_questions, only: [:index, :create, :update, :destroy]
-  resources :karma_comments, only: [:create, :update, :destroy]
+  resources :karma_comments, only: [:index, :create, :update, :destroy]
 end
