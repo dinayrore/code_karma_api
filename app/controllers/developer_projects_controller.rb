7abe823dd0c2b7d2e50fd18633cdab14befb6282
Kristine <kteich88@gmail.com>
2016-11-18 21:03:57 -0500

    Added params to include project_id, varification of user matches account.

--- app/controllers/developer_projects_controller.rb
+++ app/controllers/developer_projects_controller.rb
@@ -12,8 +12,9 @@ class DeveloperProjectsController < ApplicationController
 
   def create
     @developer_project = DeveloperProject.new developer_project_params
-    @developer_project.developer_id = @current_user.id
-    if @developer_project.save
+    @developer_project.developer_id = @current_user.account_id
+    if @developer_project.developer == @current_user.account
+      @developer_project.save
       render json: @developer_project
     else
       render json: { errors: @developer_project.errors.full_messages }, status: 422
@@ -43,6 +44,6 @@ class DeveloperProjectsController < ApplicationController
   private
 
   def developer_project_params
-    params.permit(:percentage_complete, :est_completion_date)
+    params.permit(:percentage_complete, :est_completion_date, :project_id)
   end
 end
