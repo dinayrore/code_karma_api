f5c07dde25b5b2b90b391d512489bd195648e430
Kristine <kteich88@gmail.com>
2016-11-19 13:49:42 -0500

    Added update function to client controller.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -6,7 +6,24 @@ class ClientsController < ApplicationController
       @client_dashboard_data = @client.github_oauth_data
       render json: @client_dashboard_data
     else
-      render json: { error: @client.errors.full_messages }, status: 403
+      render json: { error: 'Incorrect User' }, status: 403
+    end
+  end
+
+  def update
+    @client = User.find params[:id]
+    if @client.account_type == 'Client'
+      if !params[:organization_name].nil?
+        @client.update params[:organization_name]
+        render @client
+      elsif !params[:organization_site].nil?
+        @client.update params[:organization_name]
+        render @client
+      else
+        render @client
+      end
+    else
+      render json: { error: 'Incorrect User' }, status: 403
     end
   end
 end
