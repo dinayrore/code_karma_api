1d9c9b600afe7d214f271d4128448ea8f4364d32
Kristine <kteich88@gmail.com>
2016-11-18 21:09:37 -0500

    Updated migration.

--- db/migrate/20161117163017_create_karma_questions.rb
+++ db/migrate/20161117163017_create_karma_questions.rb
@@ -1,8 +1,8 @@
-# 
+#
 class CreateKarmaQuestions < ActiveRecord::Migration[5.0]
   def change
     create_table :karma_questions do |t|
-      t.string :karma_question
+      t.string :karma_question, null: false
       t.integer :question_like, default: 0
       t.references :developer, index: true, foreign_key: true
 
