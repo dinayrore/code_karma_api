bcd93f738f603ea7fe18f8acda3fd5c0cb9d97d4
Kristine <kteich88@gmail.com>
2016-11-18 21:09:55 -0500

    Updated migration.

--- db/migrate/20161118142306_create_karma_comments.rb
+++ db/migrate/20161118142306_create_karma_comments.rb
@@ -1,8 +1,8 @@
-# 
+#
 class CreateKarmaComments < ActiveRecord::Migration[5.0]
   def change
     create_table :karma_comments do |t|
-      t.string :karma_comment
+      t.string :karma_comment, null: false
       t.integer :comment_like, default: 0
       t.references :karma_question, index: true, foreign_key: true
       t.references :developer, index: true, foreign_key: true
