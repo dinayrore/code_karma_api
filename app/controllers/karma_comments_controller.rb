13e412ccc7c17ceee2d08b8a46d3fb09644ff19d
Kristine <kteich88@gmail.com>
2016-11-19 21:15:55 -0500

    Added index to show all questions and comments.

--- app/controllers/karma_comments_controller.rb
+++ app/controllers/karma_comments_controller.rb
@@ -1,5 +1,16 @@
 # Community Feed Comments Controller
 class KarmaCommentsController < ApplicationController
+  def index
+    @user = @current_user
+    if @user.account_type == 'Developer'
+      @questions = KarmaQuestion.all
+      @comments = KarmaComment.all
+      render :index
+    else
+      render json: { error: 'Incorrect User' }, status: 403
+    end
+  end
+
   def create
     @comment = KarmaComment.new comment_params
     @comment.developer_id = @current_user.account_id
