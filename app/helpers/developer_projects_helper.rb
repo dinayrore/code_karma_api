09de495635f9a6644103a487f5dafd10181aad96
Tim <timothyhsieh27@gmail.com>
2016-11-20 16:14:39 -0500

    Pulled previous code from Developer Projects Controller for refactoring.

--- app/helpers/developer_projects_helper.rb
+++ app/helpers/developer_projects_helper.rb
@@ -1,6 +1,43 @@
 #
 module DeveloperProjectsHelper
-  def valid?
+  def authorized
     @developer_project.developer == @current_user.account
+  end
+
+  def show_dev_projects
+    @developer_projects = DeveloperProject.where(developer_id: @current_user.account.id)
+    render json: @developer_projects
+  end
+
+  def new_dev_project
+    @developer_project = DeveloperProject.new developer_project_params
+    @developer_project.developer_id = @current_user.account.id
+  end
+
+  def save_dev_project
+    @developer_project.save
+    render json: @developer_project
+  end
+
+  def edit_dev_project
+    @developer_project.update developer_project_params
+    render json: @developer_project
+  end
+
+  def delete_dev_project
+    @developer_project.destroy
+    render json: {}, status: :ok
+  end
+
+  def find_dev_project_by_id
+    @developer_project = DeveloperProject.find params[:id]
+  end
+
+  def wrong_user_error
+    render json: { error: 'Incorrect User' }, status: 403
+  end
+
+  def wrong_syntax_error
+    render json: { errors: 'Semantically Erroneous Instructions' }, status: 422
   end
 end
