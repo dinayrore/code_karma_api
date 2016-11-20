af3454fcb6709889673451aed5a5431f7d1ae485
Kristine <kteich88@gmail.com>
2016-11-20 16:56:29 -0500

    Added update function to developers controller.

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -10,16 +10,33 @@ class DevelopersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find params[:id]
+    @developer = @user.account
+    if @user.account_type == 'Developer'
+      @developer.update developer_params
+      render json: @developer
+    else
+      render json: { error: 'Incorrect User' }, status: 403
+    end
+  end
+
   def karma
     @user = User.find params[:id]
     @question_likes = KarmaQuestion.find("#{@user.account_id}").question_like
     @comment_likes = KarmaComment.find("#{@user.account_id}").comment_like
     @karma_points = Developer.find("#{@user.account_id}").karma_points
-    if @user.account == @current_user.account
+    if @user.account_type == 'Developer'
       total_karma_points = @question_likes + @comment_likes + @karma_points
       render json: total_karma_points
     else
       render json: { error: 'Incorrect User' }, status: 403
     end
+  end
+
+  private
+
+  def developer_params
+    params.permit(:skills)
   end
 end
