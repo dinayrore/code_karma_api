8018a5ee5f6ccbaf364ccd8796a0a738587091b5
Kristine <kteich88@gmail.com>
2016-11-19 21:19:17 -0500

    Updated routes with newest controller functions/changes.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   resources :projects, only: [:index, :show, :create, :update, :destroy]
   resources :clients, only: [:show]
   resources :developers, only: [:show]
-  resources :karma_questions, only: [:index, :create, :update, :destroy]
-  resources :karma_comments, only: [:create, :update, :destroy]
+  resources :karma_questions, only: [:create, :update, :destroy]
+  resources :karma_comments, only: [:index, :create, :update, :destroy]
 end
