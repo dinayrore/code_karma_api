beb8c26c4b599af8b5ee23351a2baddc4a26ce9c
Tim <timothyhsieh27@gmail.com>
2016-11-21 15:37:29 -0500

    Deleted is_developer? action. Matches Authorized_developer? action from Developer Projects controller.

--- app/helpers/developer_projects_helper.rb
+++ app/helpers/developer_projects_helper.rb
@@ -1,6 +1,10 @@
 #
 module DeveloperProjectsHelper
-  def authorized
+  def set_current_user
+    @user = @current_user
+  end
+
+  def authorized_developer?
     @developer_project.developer == @current_user.account
   end
 
@@ -31,6 +35,58 @@ module DeveloperProjectsHelper
 
   def find_dev_project_by_id
     @developer_project = DeveloperProject.find params[:id]
+  end
+
+  def get_github_project_branches
+    find_dev_project_by_id
+    calculate_branch_request_url
+    branch_request_github
+  end
+
+  def calculate_branch_request_url
+    url = @developer_project.project_id.github_repo_url
+    owner_repo_array = url.scan(/https\:\/\/github\.com\/(\w*)\/(\w*)/).first
+    owner = owner_repo_array[0]
+    repo = owner_repo_array[1]
+    @branch_github_api_url = "https://api.github.com/#{owner}/#{repo}/branches"
+  end
+
+  def branch_request_github
+    HTTParty.get(@branch_github_api_url,
+      :headers => { 'Authorization' => "token #{@user.github_token}",
+                    'Content-Type' => 'application/json',
+                    'User-Agent' => 'Code-Karma-API' }
+    )
+  end
+
+  def post_pull_request
+    find_dev_project_by_id
+    set_current_user
+    if is_developer?
+      url = @developer_project.project.github_repo_url
+      owner_repo_array = url.scan(/https\:\/\/github\.com\/(\w*)\/(\w*)/).first
+      owner = owner_repo_array[0]
+      repo = owner_repo_array[1]
+      # pull_github_api = "https://api.github.com/repos/#{owner}/#{repo}/pulls"
+      pull_github_api = "https://api.github.com/repos/kteich88/Practice-Rspec/pulls"
+      pull_title = "Kristine you should totally accept this Amazing Pull Request."
+      head_branch = "master"
+      base_branch = "new_test"
+      pull_body = "Like seriously plz accept it."
+      e = HTTParty.post(pull_github_api,
+      :headers => { 'Authorization' => "token #{@user.github_token}",
+                    'Content-Type' => 'application/json',
+                    'User-Agent' => 'Code-Karma-API'},
+
+      :query =>    { 'title' => "#{pull_title}",
+                    'base' => "#{base_branch}",
+                    'head' => "#{head_branch}",
+                    'body' => "#{pull_body}"}
+      )
+      binding.pry
+    else
+      wrong_syntax_error
+    end
   end
 
   def wrong_user_error
