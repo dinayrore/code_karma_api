cc032daa14569f3ec9bd4b30a3342558958ee289
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-22 15:07:19 -0500

    karma questions helper revised

--- app/helpers/karma_questions_helper.rb
+++ app/helpers/karma_questions_helper.rb
@@ -1,39 +1,48 @@
 # Included in KarmaQuestionsController
 module KarmaQuestionsHelper
-  def create_question
+
+  def create_new_question
     @question = KarmaQuestion.new question_params
+  end
+
+  def confirm_account_id
     @question.developer_id = @current_user.account_id
   end
 
-  def confirm_or_deny_creation_and_render
-    if @question.developer == @current_user.account
-      @question.save
-      render json: @question
-    else
-      render json: { errors: 'Semantically Erroneous Instructions' }, status: 422
-    end
+  def confirm_developer_is_user
+    @question.developer == @current_user.account
+  end
+
+  def save_question
+    @question.save
+  end
+
+  def render_json
+    render json: @question
+  end
+
+  def render_erronious_instruction_error
+    render json: { errors: 'Semantically Erroneous Instructions' }, status: 422
   end
 
   def find_a_question
     @question = KarmaQuestion.find params[:id]
   end
 
-  def confirm_or_deny_update_and_render
-    if @question.developer == @current_user.account
-      @question.update question_params
-      render json: @question
-    else
-      render json: { error: 'Incorrect User' }, status: 403
-    end
+  def update_question
+    @question.update question_params
   end
 
-  def confirm_or_deny_like_and_render
-    if @question.developer.user.account_type == 'Developer'
-      @question.update(question_like: params[:question_like].to_i + 1)
-      render json: @question
-    else
-      render json: { errors: 'Semantically Erroneous Instructions' }, status: 422
-    end
+  def render_incorrect_user_error
+    render json: { error: 'Incorrect User' }, status: 403
+  end
+
+  def confirm_account_type_developer
+    @question.developer.user.account_type == 'Developer'
+  end
+
+  def increment_question_like
+    @question.update(question_like: params[:question_like].to_i + 1)
   end
 
   private
