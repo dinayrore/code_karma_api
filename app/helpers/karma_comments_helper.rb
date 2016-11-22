b498640e189c7d6d4d38c1d2c459a307d3ead7e6
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-22 11:32:44 -0500

    questions and comments helpers ready to substitute into controllers

--- app/helpers/karma_comments_helper.rb
+++ app/helpers/karma_comments_helper.rb
@@ -3,66 +3,59 @@ module KarmCommentsHelper
     @user = @current_user
   end
 
-  def if_current_user_is_developer
+  def show_all_comments_and_questions
     if @user.account_type == 'Developer'
-  end
-
-  def render_all_comments_and_questions
-    @questions = KarmaQuestion.all
-    render 'index.json.jbuilder'
-  end
-
-  def render_incorrect_user_error
+      @questions = KarmaQuestion.all
+      render 'index.json.jbuilder'
     else
       render json: { error: 'Incorrect User' }, status: 403
     end
   end
 
-  def create_comment
+  def find_a_comment
     @comment = KarmaComment.new comment_params
-    @comment.developer_id = @current_user.account_id
   end
 
-  def confirm_developer_and_save
+  def confirm_or_deny_developer_and_save_comment
+    @comment.developer_id = @current_user.account_id
     if @comment.developer == @current_user.account
       @comment.save
       render json: @comment
-  end
-
-  def render_instruction_error
     else
       render json: { errors: 'Semantically Erroneous Instructions' }, status: 422
     end
   end
 
-  def edit_comment
-    @comment = KarmaComment.find params[:id]
-  end
-
-  def confirm_developer_and_save_update
+  def confirm_or_deny_developer_and_render_update
     if @comment.developer == @current_user.account
       @comment.update(karma_comment: params[:karma_comment])
       render json: @comment
-  end
-  #render incorrect user error
-  def destroy_comment
-    @comment = KarmaComment.find params[:id]
+    else
+      render json: { error: 'Incorrect User' }, status: 403
+    end
   end
 
-  def confirm_developer_for_destroy
+  def confirm_or_deny_developer_and_destroy_comment
     if @comment.developer == @current_user.account
       @comment.destroy
       render json: {}, status: :ok
-  end
-  #render incorrect user error
-  def like_a_comment
-    @comment = KarmaComment.find params[:id]
+    else
+      render json: { error: 'Incorrect User' }, status: 403
+    end
   end
 
-  def confirm_developer_increment_like
+  def confirm_or_deny_developer_increment_like
     if @comment.developer.user.account_type == 'Developer'
       @comment.update(comment_like: params[:comment_like].to_i + 1)
       render json: @comment
+    else
+      render json: { errors: 'Semantically Erroneous Instructions' }, status: 422
+    end
   end
-  #render instruction error
+
+  private
+
+  def comment_params
+    params.permit(:karma_comment, :karma_question_id)
+  end
 end
