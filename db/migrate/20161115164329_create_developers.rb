15878a30ea8d9e48e3d28eba48264552fdd6b8dc
Kristine <kteich88@gmail.com>
2016-11-27 16:44:24 -0500

    Updated column in developers table to include commit count.

--- db/migrate/20161115164329_create_developers.rb
+++ db/migrate/20161115164329_create_developers.rb
@@ -3,6 +3,7 @@ class CreateDevelopers < ActiveRecord::Migration[5.0]
   def change
     create_table :developers do |t|
       t.integer :karma_points, default: 0
+      t.integer :commits, default: 0
       t.string :skills
 
       t.timestamps
