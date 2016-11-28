b22e084f0001d7e950f7084a17430d7440851699
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-28 09:41:48 -0500

    tests outlined for karma questions controller

--- test/controllers/karma_questions_controller_test.rb
+++ test/controllers/karma_questions_controller_test.rb
@@ -1,4 +1,51 @@
 require 'test_helper'
 
 class KarmaQuestionsControllerTest < ActionDispatch::IntegrationTest
+  test 'Question contains text' do
+
+  end
+
+  test 'Saves question if current user is developer' do
+
+  end
+
+  test 'Renders user error if current user is not developer' do
+
+  end
+
+  test 'Question being edited is identified by id' do
+
+  end
+
+  test 'Saves update if the current user is the author of the question' do
+
+  end
+
+  test 'Renders user error if the developer updating the question is not the author' do
+
+  end
+
+  test 'Question being destroyed is identified by id' do
+
+  end
+
+  test 'Delete question if the current user is the author of the question' do
+
+  end
+
+  test 'Renders user error if the developer deleting the question is not the author' do
+
+  end
+
+  test 'Question being liked is identied by id' do
+
+  end
+
+  test 'If the current user is a developer increment question like' do
+
+  end
+
+  test 'Renders user error if user is not developer' do
+
+  end
 end
