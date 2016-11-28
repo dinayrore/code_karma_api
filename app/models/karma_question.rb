eb3a81ae47603113c3696d907c9fd634ae4edc0d
Tim <timothyhsieh27@gmail.com>
2016-11-28 11:30:11 -0500

    Removed erroneous dependent destroy on Karma Comment owner, which is Karma Question.

--- app/models/karma_question.rb
+++ app/models/karma_question.rb
@@ -1,5 +1,5 @@
 # Belongs to a Developer and has many comments
 class KarmaQuestion < ApplicationRecord
   belongs_to :developer
-  has_many :karma_comment, dependent: :destroy
+  has_many :karma_comment
 end
