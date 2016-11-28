451f95bc5d8487a58f164f042200f5fe03e46ca5
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-28 09:57:16 -0500

    minitest questions created for karma comments controller

--- test/controllers/karma_comments_controller_test.rb
+++ test/controllers/karma_comments_controller_test.rb
@@ -1,4 +1,71 @@
 require 'test_helper'
 
 class KarmaCommentsControllerTest < ActionDispatch::IntegrationTest
+  test 'The user is verified as the current user' do
+
+  end
+
+  test 'The current user account type is developer' do
+
+  end
+
+  test 'Show all questions and comments if account type is verified' do
+
+  end
+
+  test 'Render user error if account type is not developer' do
+
+  end
+
+  test 'Comment created contains text' do
+
+  end
+
+  test 'Developer id is equal to the current users account id' do
+
+  end
+
+  test 'Save comment is the current users account type is developer' do
+
+  end
+
+  test 'Render syntax error if current users account type is not developer' do
+
+  end
+
+  test 'Comment is identified by id' do
+
+  end
+
+  test 'Save update if the comments author is the current user' do
+
+  end
+
+  test 'Render user error if comments author is not current user' do
+
+  end
+
+  test 'Comment is identified by id' do
+
+  end
+
+  test 'Delete comment if the comments author is the current user' do
+
+  end
+
+  test 'Render user error if comments author is not current user' do
+
+  end
+
+  test 'Comment is identified by id' do
+
+  end
+
+  test 'Increment like if current users account type is developer' do
+    
+  end
+
+  test 'Render user error if current users account type is not developer' do
+
+  end
 end
