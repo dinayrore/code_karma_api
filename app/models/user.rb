d687fb19995b0c4103d6563e26b6578fbb25b119
Kristine <kteich88@gmail.com>
2016-11-28 15:37:42 -0500

    Updated model to include validations for all user specific data.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 # Users can be Clients or Developers (polymorphic)
 class User < ApplicationRecord
   belongs_to :account, polymorphic: true
+  validates :code_karma_token, :github_token, :github_oauth_data, :email, presence: true
 end
