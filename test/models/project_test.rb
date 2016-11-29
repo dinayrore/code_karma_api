108753c7558a2494d197312c258eda7f9992772d
Kristine <kteich88@gmail.com>
2016-11-29 09:26:07 -0500

    Updated test file.

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,12 +1,17 @@
 require 'test_helper'
-
+#
 class ProjectTest < ActiveSupport::TestCase
+  def setup
+    @client = Client.new
+    @project = Project.new(title: 'Project Title', brief_description: 'brief', description: 'more', github_repo_url: github.com, active_site_url: active-site.com, fulfilled: false, fix_type: 'bug fix', languages: {'Ruby' => 100})
+  end
+
   test 'Belongs to relationship with client model' do
 
   end
 
-  test 'Has many relationship with developer project model' do
-
+  test '#developer_project' do
+    assert true, @client.project.developer_project
   end
 
   test 'Dependant destroy relationship with developer project model' do
